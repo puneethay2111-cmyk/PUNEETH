6f855bffc82f12f60841e726097a9e5bb0ddee69
Puneeth <puneethay2111@gmail.com>
2026-09-24 16:53:13 +0530

    Update arr.java with new logic

---
+++ arr.java
@@ -0,0 +1,19 @@
+public class arr 
+{
+  public static void main(String [] args)   
+  {
+    int m=0;
+    int sum=0;
+    int num[]={5,12,16,19,21,1};
+    for (int i=0;i<6;i++)
+    {
+      sum +=num[i];
+      System.out.print(num[i]+"\n");
+  
+  }
+      m=sum/6;
+      
+      System.out.print(sum+"\n");
+       System.out.print(m+"\n");
+    }
+}
