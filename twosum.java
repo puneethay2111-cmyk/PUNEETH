90bd146f6aeb6d9deb420a9dbf54d3a700eaedb4
Puneeth <puneethay2111@gmail.com>
2026-09-24 20:13:42 +0530

    Add twosum.java and update twosum.java

--- twosum.java
+++ twosum.java
@@ -4,13 +4,23 @@ public class twosum
    {
     int a[]={1,3,5,9,4,6};
     int target=3;
+    boolean is=false;
     for(int i=0;i<a.length;i++)
     {
         System.out.print(a[i]+"\n");
         if(a[i]==target)
         {
             System.out.print("target is in a"+" "+"\n");
+           is=true;
         }
+     }
+     if (is)
+     {
+        System.out.println("is in a");
+     }
+     else
+     {
+        System.out.print("is not in a");
      }
    }
 }
