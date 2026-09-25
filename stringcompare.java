ec18760d4176de85588135baacfbb2e9fd12251e
Puneeth <puneethay2111@gmail.com>
2026-09-25 10:48:27 +0530

    Add stringcompare.java and update other files

---
+++ stringcompare.java
@@ -0,0 +1,15 @@
+public class stringcompare 
+{
+  public static void main( String[] args)
+  {
+    String n1=new String("punee");
+    String n2=new String("punee");
+    String n3="punee";
+    String n4="punee";
+    System.out.print(n1==n2);
+    System.out.println("  ");
+    System.out.print(n2==n1);
+    System.out.println("  ");
+    System.out.print(n3==n4);
+  }   
+}
