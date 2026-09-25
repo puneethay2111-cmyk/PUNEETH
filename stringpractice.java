da82e7a6dab3040af5a9d16e5b216491ecd2da5e
Puneeth <puneethay2111@gmail.com>
2026-09-25 12:56:07 +0530

    Add stringpractice.java

---
+++ stringpractice.java
@@ -0,0 +1,16 @@
+public class stringpractice 
+{
+  public static void main(String [] args)
+  {
+    String city ="Bangalore";
+    System.out.print(city.length()+"\n");
+    System.out.print(city.toUpperCase()+"\n");
+    System.out.print(city.equals("Bangalore"+"\n"));
+    //String fiest ="JAVA";
+   // String second="programing";
+   //String combined=fiest+second;
+   // String withNumber="score :"+95;
+    
+  }
+
+}
