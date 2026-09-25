0f6478a82a00be62771102ffe98b860ac473cdf8
Puneeth <puneethay2111@gmail.com>
2026-09-25 14:59:28 +0530

    Add factorial.java

--- factorial.java
+++ factorial.java
@@ -3,12 +3,12 @@ public class factorial
     public static void main(String[] args) 
     {
         int num=5;
-        int fact=1;
+        int fa=1;
         for(int i=1;i<=num;i++)
         {
-            fact=fact*i;
+            fa=fa*i;
         }
-        System.out.println("Factorial of "+num+" is "+fact);
+        System.out.println("Factorial of "+num+" is "+fa);
     }
     
 }
